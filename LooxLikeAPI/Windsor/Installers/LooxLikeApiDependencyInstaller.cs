c5542310a443d57c99802b4ba776ac4640f26473
Raffaele Lovino <raffaele.lovino.intern@yoox.com>
2015-10-02 17:31:12 +0200

    changed connection string

--- LooxLikeAPI/Windsor/Installers/LooxLikeApiDependencyInstaller.cs
+++ LooxLikeAPI/Windsor/Installers/LooxLikeApiDependencyInstaller.cs
@@ -54,7 +54,8 @@ namespace LooxLikeAPI.Windsor.Installer
 
             return Database.OpenFile(DATABASE_NAME);*/
 
-            return Database.OpenConnection("data source=looxlike-aws-db.cgh0nwmobyrc.eu-central-1.rds.amazonaws.com;initial catalog=LooxLikeDB;user id=looxlike_admin;password=LaPa$$w0rdDB");
+            //return Database.OpenConnection("data source=looxlike-aws-db.cgh0nwmobyrc.eu-central-1.rds.amazonaws.com;initial catalog=LooxLikeDB;user id=looxlike_admin;password=LaPa$$w0rdDB");
+			return Database.OpenConnection("data source=.;initial catalog=LooxLikeDB;user id=looxlikelogin;password=LaPa$$w0rdDB");
         }
 
 
