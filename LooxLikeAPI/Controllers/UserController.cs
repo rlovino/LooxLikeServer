a7b53430415594e652907b78b9fbce1d2b818924
yazanin <alessandro.zanin.intern@yoox.com>
2015-10-23 15:19:35 +0200

    create the future of basic auth

--- LooxLikeAPI/Controllers/UserController.cs
+++ LooxLikeAPI/Controllers/UserController.cs
@@ -24,31 +24,5 @@ namespace LooxLikeAPI.Controllers
 
         }
 
-		//// GET: api/User
-		//public IEnumerable<string> Get()
-		//{
-		//	return new string[] { "value1", "value2" };
-		//}
-
-		//// GET: api/User/5
-		//public string Get(int id)
-		//{
-		//	return "value";
-		//}
-
-		//// POST: api/User
-		//public void Post([FromBody]string value)
-		//{
-		//}
-
-		//// PUT: api/User/5
-		//public void Put(int id, [FromBody]string value)
-		//{
-		//}
-
-		//// DELETE: api/User/5
-		//public void Delete(int id)
-		//{
-		//}
     }
 }
