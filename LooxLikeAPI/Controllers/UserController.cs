b86f44f515332e5418b6f1585245cd369a3702fa
Raffaele Lovino <raffaele.lovino.intern@yoox.com>
2015-10-29 10:16:50 +0100

    created user controller draft (sid)

---
+++ LooxLikeAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LooxLikeAPI.Services;
+
+namespace LooxLikeAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+	    private IUserService _userService;
+
+	    public UserController(IUserService userService)
+	    {
+		    _userService = userService;
+	    }
+
+
+		[Route("user/{username:string}")]
+        public HttpResponseMessage Get(string username)
+        {
+            var user = _userService.GetUser(username);
+			HttpResponseMessage responseMessage = new HttpResponseMessage(System.Net.HttpStatusCode.Redirect);
+				
+        }
+
+
+    }
+}
